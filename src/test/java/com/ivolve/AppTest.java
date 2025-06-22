a90b8c266b458ce77d3071e2e5ed6145d8b8e710
Ibrahim-Adel15 <ibrahim.adel@ivolve.io>
2025-06-22 04:39:34 -0700

    Add files via upload

---
+++ src/test/java/com/ivolve/AppTest.java
@@ -0,0 +1,12 @@
+package com.ivolve;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testOutput() {
+        assertEquals("Hello iVolve Trainee", "Hello iVolve Trainee"); // simple pass test
+    }
+}
